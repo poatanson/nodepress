cfc7b9d63468e9874b3bb072067b9c2e0f8f6c11
손유승 <youseung5153@gmail.com>
2025-04-06 05:31:37 +0900

    modified

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,22 @@
+// 📄 routes/posts.js
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+// 전체 게시글 목록
 router.get('/', async (req, res) => {
     const posts = await Post.find().populate('author', 'name');
     res.render('posts', { posts });
 });
 
+// 게시글 상세 보기
+router.get('/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id).populate('author', 'name');
+    if (!post) return res.status(404).send('Post not found');
+    res.render('postDetail', { post });
+});
+
+// 게시글 추가
 router.post('/add', async (req, res) => {
     const { title, content } = req.body;
     await Post.create({
@@ -17,6 +27,7 @@ router.post('/add', async (req, res) => {
     res.redirect('/posts');
 });
 
+// 게시글 삭제
 router.post('/delete/:id', async (req, res) => {
     await Post.findByIdAndDelete(req.params.id); 
     res.redirect('/posts');
